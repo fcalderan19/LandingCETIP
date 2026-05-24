5be7e5a99dd3fb80a30c10634830d0ea36d6fb35
fcalderan19 <fcalderan@fi.uba.ar>
2026-05-24 18:25:02 -0300

    fix(cms): mover autoUpdates a deployment.autoUpdates

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,6 +5,7 @@ export default defineCliConfig({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production"
   },
-  /** Permite que el CLI lea variables desde .env.local además de .env */
-  autoUpdates: true
+  deployment: {
+    autoUpdates: true
+  }
 });
